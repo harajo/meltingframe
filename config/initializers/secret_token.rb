1d10ce5213662cfa736b7482b9a79bf7f20d04a1
hara <beautycho511@daum.net>
2015-10-08 11:19:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meltingframe::Application.config.secret_key_base = 'f5c8f452f45dc82282e91bebcbca3877417b4d1b64e9df0aba75ec4ccfc09c41660d1134ac73d1bf68575cd8ab5efdb2300416f2ba2811b8b5c4a94b7336cf55'
