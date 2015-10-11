16b9ac6f11751baf04ca849f6c43e0bf08b1476b
EJ <eunji91park@gmail.com>
2015-10-11 06:01:31 +0000

    이거저것 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Meltingframe::Application.routes.draw do
+	get 'home/home' => 'home#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
